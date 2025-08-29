e05d3cca5b36cc18e52c3570fd43c5b01ea14daa
Techaphatr INDHAVIVADHANA <techaphatr.i@ku.th>
2025-08-29 21:39:52 +0700

    Init backend and setup express.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const { text } = require('body-parser');
+
+const app = express();
+const PORT = 15000; // Feel free to change port here.
+
+const mongoURL = process.env.MONGO_URL || 'mongodb://localhost:27017/flearn';
+
+app.use(cors());
+app.use(express.json());
